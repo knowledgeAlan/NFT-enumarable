d72e757065c166d88e52a32dd0bdc217101df3be
zhongzuomin <1299076979@qq.com>
2024-01-14 15:02:03 +0800

    ERC721Enumerable implement

--- contract/NFT-enum.sol
+++ contract/NFT-enum.sol
@@ -1,7 +1,37 @@
 // SPDX-License-Identifier:  GPL-3.0
+pragma solidity ^0.8.20;
 
-contract NFTEnum {
-    constructor() {
-        
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+
+contract NFTEnum is ERC721, ERC721Enumerable, ERC721Burnable {
+    constructor() ERC721("ZCDToken0114", "ZT0114") {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
